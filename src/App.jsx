7144ee931db129078e8223c2ca6e5c8ba55ed734
Anastasiia <95031679+anastasiiaal@users.noreply.github.com>
2023-07-24 16:58:32 +0200

    🍴starter files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <section class="section">
+      <div class="container">
+        <h1 class="title">
+          Hello World
+        </h1>
+        <p class="subtitle">
+          My first website with <strong>Bulma</strong>!
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default App
