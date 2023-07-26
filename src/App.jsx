cf875704335821fafba412427070e61e5e7527b3
Anastasiia <95031679+anastasiiaal@users.noreply.github.com>
2023-07-26 17:09:08 +0200

    :bookmark_tabs: form html +/- completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,28 @@ function App() {
       <div className="container center">
 
         <form className="notification card">
+          
+          <h1 className="title is-4 mb-5">Please fill in your data:</h1>
 
           <div className="columns">
             <div className="column">
               <div className="field">
-                <label className="label">Age</label>
+                <label className="label" for="age">Age</label>
                 <div className="control">
-                  <input className="input" type="number" placeholder="Your age" />
+                  <input id="age" className="input" type="number" placeholder="Your age" />
                 </div>
               </div>
             </div>
             <div className="column">
               <div className="field">
                 <div className="control">
-                  <label className="label">Sex</label>
+                  <label className="label">Biological sex</label>
                   <label className="radio">
-                    <input type="radio" name="question" />
+                    <input type="radio" name="sex" />
                     Male
                   </label>
                   <label className="radio">
-                    <input type="radio" name="question" />
+                    <input type="radio" name="sex" />
                     Female
                   </label>
                 </div>
@@ -34,9 +36,9 @@ function App() {
           <div className="columns">
             <div className="column">
               <div className="field">
-                <label className="label">Weight</label>
+                <label className="label" for="weight">Weight</label>
                 <div className="control has-icons-left">
-                  <input className="input" type="number" placeholder="Your weight in kg" />
+                  <input id="weight" className="input" type="number" placeholder="Your weight in kg" />
                   <span className="icon is-small is-left">
                     <i className="fa-solid fa-weight-scale"></i>
                   </span>
@@ -46,14 +48,72 @@ function App() {
 
             <div className="column">
               <div className="field">
-                <label className="label">Height</label>
+                <label className="label" for="height">Height</label>
                 <div className="control has-icons-left">
-                  <input className="input" type="number" placeholder="Your height in cm" />
+                  <input id="height" className="input" type="number" placeholder="Your height in cm" />
                   <span className="icon is-small is-left">
                     <i className="fa-solid fa-up-down"></i>
                   </span>
                 </div>
               </div>
+            </div>
+          </div>
+
+          <div className="columns">
+            <div className="column">
+              <div className="field">
+                <div className="control">
+                  <label className="label">Weekly activity</label>
+                  <div className="vertical">
+                    <label className="radio">
+                      <input type="radio" name="activity" />
+                      Sedentary: little or no excercise
+                    </label>
+                    <label className="radio">
+                      <input type="radio" name="activity" />
+                      Light: exercise 1-3 times/week
+                    </label>
+                    <label className="radio">
+                      <input type="radio" name="activity" />
+                      Moderate: exercise 4-6 times/week
+                    </label>
+                    <label className="radio">
+                      <input type="radio" name="activity" />
+                      Intense: exercise 7+ times/week
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="columns">
+            <div className="column">
+              <div className="field">
+                <div className="control">
+                  <label className="label">Your goal is to..</label>
+                  <div className="vertical">
+                    <label className="radio">
+                      <input type="radio" name="goal" />
+                      Lose weight
+                    </label>
+                    <label className="radio">
+                      <input type="radio" name="goal" />
+                      Maintain weight
+                    </label>
+                    <label className="radio">
+                      <input type="radio" name="goal" />
+                      Gain weight
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="field">
+            <div className="control">
+              <button className="button is-link mb-2">See results</button>
             </div>
           </div>
 
